e355a54276592dbcd290999ee2d17181a12f1a05
HieeusNguyen <hieudeptrai4603@gmail.com>
2025-03-24 22:31:08 +0700

    Update package.json

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/amazon",
+    MONGODB_URL: process.env.MONGODB_URL || "mongodb://127.0.0.1/ecommerce_website",
     JWT_SECRET: process.env.JWT_SECRET || "somethingsecret"
 };
